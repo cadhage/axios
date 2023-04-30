10c2dfca9a98f48a5be2aca017586b3bfcb1fa6c
unknown <cadhage@gmail.com>
2023-04-30 13:05:11 +0530

    adding starter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import Title from './components/Title';
+import Title from "./components/Title";
+import Setup1 from "./examples/1-first-request";
+import Setup2 from "./examples/2-headers";
+import Setup3 from "./examples/3-post-request";
+import Setup4 from "./examples/4-global-instance";
+import Setup5 from "./examples/5-custom-instance";
+import Setup6 from "./examples/6-interceptors";
+
 function App() {
   return (
     <main>
       <Title />
+      <Setup4 />
     </main>
   );
 }
